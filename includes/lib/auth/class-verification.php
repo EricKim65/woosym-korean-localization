b8b0c3ed972ef00ac237ffc199d9223864226c95
changwoo <ep6tri@hotmail.com>
2015-12-04 13:39:49 +0900

    인증 실패의 경우 체크아웃 버튼은 살리고, 신용카드 결제 옵션을 삭제하도록 로직 수정.

--- includes/lib/auth/class-verification.php
+++ includes/lib/auth/class-verification.php
@@ -11,6 +11,8 @@ use wskl\lib\cassandra\OrderItemRelation;
 
 class Verification {
 
+	private $verified;
+
 	public function __construct() {
 
 		$this->initialize();
@@ -23,6 +25,13 @@ class Verification {
 		 * @see woocommerce/includes/wc-template-functions.php woocommerce_checkout_payment()
 		 */
 		add_action( 'woocommerce_review_order_before_submit', array( $this, 'show_unverified_warning' ) );
+
+		/**
+		 * @see woocommerce/includes/class-wc-payment-gateways.php get_available_payment_gateways()
+		 */
+		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'check_payment_gateways' ) );
+
+		$this->verified = $this->verify();
 	}
 
 	public function show_unverified_warning() {
@@ -30,6 +39,43 @@ class Verification {
 		if( ! is_checkout() ) {
 			return;
 		}
+
+		if( ! $this->verified ) {
+
+			$default_path = WSKL_PATH . '/includes/lib/auth/templates/';
+			wc_get_template( 'checkout-unauthorized.php', array(), '', $default_path );
+			// add_filter( 'woocommerce_order_button_html', array( $this, 'remove_submit' ), 999, 0 );
+		}
+	}
+
+//	public function remove_submit() {
+//		return '';
+//	}
+
+	public function check_payment_gateways( array $available_gateways ) {
+
+		/** @var \WC_Payment_Gateway $gateway */
+		foreach( $available_gateways as $idx => $gateway ) {
+
+			if( !( $gateway instanceof \WC_Payment_Gateway ) ) {
+				continue;
+			}
+
+			if( FALSE === array_search( $gateway->id, array( 'ags_credit', 'kcp_credit', 'inicis_credit' ) ) ) {
+				continue;
+			}
+
+			if( $this->verified ) {
+				continue;
+			}
+
+			unset( $available_gateways[ $idx ] );
+		}
+
+		return $available_gateways;
+	}
+
+	private function verify() {
 
 		$info = new Auth_Model( 'payment' );
 
@@ -41,19 +87,21 @@ class Verification {
 		$verification = ClientAPI::verify( $key_type, $key_value, $site_url );
 
 		if ( $verification instanceof OrderItemRelation ) {
+
 			$info->set_value( $verification );
 			$info->save();
+
+			return TRUE;
+
+		} else if( $verification === NULL ) { // 인증 실패
+
+			return FALSE;
+
+		} else if ( $verification === FALSE ) { // 인증 불가.
+
+			return $info->is_verified();
 		}
 
-		else if( $verification === NULL || !$info->is_verified() ) {
-
-			$default_path = WSKL_PATH . '/includes/lib/auth/templates/';
-			wc_get_template( 'checkout-unauthorized.php', array(), '', $default_path );
-			add_filter( 'woocommerce_order_button_html', array( $this, 'remove_submit' ), 999, 0 );
-		}
-	}
-
-	public function remove_submit() {
-		return '';
+		return FALSE;
 	}
 }
