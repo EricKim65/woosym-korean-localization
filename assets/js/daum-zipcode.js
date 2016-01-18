f5a1c4b717a9ccd44d820e87e8cec00c28b1c5b3
changwoo <ep6tri@hotmail.com>
2016-01-18 16:10:05 +0900

    다음의 새로운 주소찾기 api를 적용.

--- assets/js/daum-zipcode.js
+++ assets/js/daum-zipcode.js
@@ -1,26 +1,31 @@
-	if (document.getElementById("billing_postcode")) {  // If·Î ÂÉ°³¾î ³õÀº °Ç ´Ù ÀÌÀ¯°¡ ÀÖ½¿.
-		document.getElementById("billing_postcode").readOnly= true;
-		document.getElementById("billing_address_1").readOnly=true;
-		document.getElementById("billing_zipcode_button").onclick=function(){ 
-			openDaumPostcode("billing");
-		};
-	}
+if (document.getElementById("billing_postcode")) {  // Ifï¿½ï¿½ ï¿½É°ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ö½ï¿½.
+    document.getElementById("billing_postcode").readOnly = true;
+    document.getElementById("billing_address_1").readOnly = true;
+    document.getElementById("billing_zipcode_button").onclick = function () {
+        openDaumPostcode("billing");
+    };
+}
 
-	if (document.getElementById("shipping_postcode")) {
-		document.getElementById("shipping_postcode").readOnly= true;
-		document.getElementById("shipping_address_1").readOnly=true;
-		document.getElementById("shipping_zipcode_button").onclick=function(){ 
-			openDaumPostcode("shipping");
-		};
-	}
+if (document.getElementById("shipping_postcode")) {
+    document.getElementById("shipping_postcode").readOnly = true;
+    document.getElementById("shipping_address_1").readOnly = true;
+    document.getElementById("shipping_zipcode_button").onclick = function () {
+        openDaumPostcode("shipping");
+    };
+}
 
-	 function openDaumPostcode(billship) {
-        new daum.Postcode({
-             oncomplete: function(data) {
-                 document.getElementById(billship+"_postcode").value = data.postcode1+"-"+data.postcode2;
-                 document.getElementById(billship+"_address_1").value = data.address1+" "+data.address2;
-                 //document.getElementById("jibeon").value = data.relatedAddress;
-                 document.getElementById(billship+"_address_2").focus();
-             }
-         }).open();
-     }
+function openDaumPostcode(billship) {
+    new daum.Postcode({
+        oncomplete: function (data) {
+            if(data.userSelectedType == 'J') {
+                document.getElementById(billship + "_postcode").value = data.postcode;
+                document.getElementById(billship + "_address_1").value = data.jibunAddress;
+            } else if(data.userSelectedType == 'R') {
+                document.getElementById(billship + "_postcode").value = data.zonecode;
+                document.getElementById(billship + "_address_1").value = data.roadAddress;
+            }
+            //document.getElementById("jibeon").value = data.relatedAddress;
+            document.getElementById(billship + "_address_2").focus();
+        }
+    }).open();
+}
