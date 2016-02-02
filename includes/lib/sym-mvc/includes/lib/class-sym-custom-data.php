a0470be369b5572974112de1f979cd011a6c3ace
changwoo <ep6tri@hotmail.com>
2016-02-02 10:59:51 +0900

    reformat

--- includes/lib/sym-mvc/includes/lib/class-sym-custom-data.php
+++ includes/lib/sym-mvc/includes/lib/class-sym-custom-data.php
@@ -1,67 +1,89 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-class Sym_Custom_Data
-{
-    private $order_id;
-    private $fields;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-    public static function extend(&$order) {
-        $order->custom = new Sym_Custom_Data($order->id);
-    }
 
-    private function __construct($order_id) {
-        $this->order_id = $order_id;
-        $this->fields = array();
+class Sym_Custom_Data {
 
-        // get data from the database
-        $this->populate();
-    }
+	private $order_id;
+	private $fields;
 
-    private function populate() {
-        global $wpdb;
+	public static function extend( &$order ) {
 
-        $fields = $wpdb->get_results(
-            'SELECT * FROM sym_custom_data WHERE order_id = ' . $this->order_id,
-            OBJECT
-        );
+		$order->custom = new Sym_Custom_Data( $order->id );
+	}
 
-        foreach($fields as $field) {
-            $value = json_decode($field->custom_value,true);
+	private function __construct( $order_id ) {
 
-            if($value != null) $this->fields[$field->custom_key] = $value;
-            else $this->fields[$field->custom_key] = $field->custom_value;
-        }
-    }
+		$this->order_id = $order_id;
+		$this->fields   = array();
 
-    public function save() {
-        global $wpdb;
+		// get data from the database
+		$this->populate();
+	}
 
-        $wpdb->query('DELETE FROM sym_custom_data WHERE order_id = ' . $this->order_id);
+	private function populate() {
 
-        foreach($this->fields as $key => $value) {
-            if(empty($value)) continue;
+		global $wpdb;
 
-            if(is_object($value) || is_array($value)) $value = json_encode($value);
+		$fields = $wpdb->get_results(
+			'SELECT * FROM sym_custom_data WHERE order_id = ' . $this->order_id,
+			OBJECT
+		);
 
-            $wpdb->insert('sym_custom_data',array(
-                'order_id' => $this->order_id,
-                'custom_key' => $key,
-                'custom_value' => $value,
-            ),array('%d','%s','%s'));
-        }
-    }
+		foreach ( $fields as $field ) {
+			$value = json_decode( $field->custom_value, true );
 
-    public function __isset($key) {
-        return isset($this->fields[$key]);
-    }
-    public function __unset($key) {
-        unset($this->fields[$key]);
-    }
-    public function __get($key) {
-        return isset($this->fields[$key]) ? $this->fields[$key] : null;
-    }
-    public function __set($key,$value) {
-        $this->fields[$key] = $value;
-    }
+			if ( $value != null ) {
+				$this->fields[ $field->custom_key ] = $value;
+			} else {
+				$this->fields[ $field->custom_key ] = $field->custom_value;
+			}
+		}
+	}
+
+	public function save() {
+
+		global $wpdb;
+
+		$wpdb->query( 'DELETE FROM sym_custom_data WHERE order_id = ' . $this->order_id );
+
+		foreach ( $this->fields as $key => $value ) {
+			if ( empty( $value ) ) {
+				continue;
+			}
+
+			if ( is_object( $value ) || is_array( $value ) ) {
+				$value = json_encode( $value );
+			}
+
+			$wpdb->insert( 'sym_custom_data', array(
+				'order_id'     => $this->order_id,
+				'custom_key'   => $key,
+				'custom_value' => $value,
+			), array( '%d', '%s', '%s' ) );
+		}
+	}
+
+	public function __isset( $key ) {
+
+		return isset( $this->fields[ $key ] );
+	}
+
+	public function __unset( $key ) {
+
+		unset( $this->fields[ $key ] );
+	}
+
+	public function __get( $key ) {
+
+		return isset( $this->fields[ $key ] ) ? $this->fields[ $key ] : null;
+	}
+
+	public function __set( $key, $value ) {
+
+		$this->fields[ $key ] = $value;
+	}
 }
