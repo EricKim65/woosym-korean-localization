3546e283c62d7d8f3a38874d5161e9a3ad536f8e
changwoo <ep6tri@hotmail.com>
2016-02-11 16:29:24 +0900

    file inclusion을 하나의 모듈 로딩으로 이해하도록 헬퍼 메소드 wskl_load_module 추가.

--- includes/lib/wskl-functions.php
+++ includes/lib/wskl-functions.php
@@ -29,3 +29,23 @@ function wskl_is_option_enabled( $option_name ) {
 
 	return filter_var( $value, FILTER_VALIDATE_BOOLEAN );
 }
+
+
+/**
+ * 또다른 helper function
+ *
+ * @param string $relative_path  WSKL_PATH 로부터 상대적인 경로. 앞에 '/' 가 안 붙었으면 자동으로 붙임.
+ * @param string $option_name    prefix 문자열을 붙이지 않은 옵션 이름. 빈문자열인 경우는 옵션을 체크하지 않음.
+ */
+function wskl_load_module( $relative_path, $option_name = '' ) {
+
+	if( empty( $option_name ) || wskl_is_option_enabled( $option_name ) ) {
+
+		if( $relative_path[0] != '/' ) {
+			$relative_path = '/' . $relative_path;
+		}
+
+		/** @noinspection PhpIncludeInspection */
+		require_once( WSKL_PATH . $relative_path );
+	}
+}
