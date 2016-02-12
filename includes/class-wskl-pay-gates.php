707da2a9d6c8e304cbde265eb347bd20cd8a2f20
changwoo <ep6tri@hotmail.com>
2016-02-12 14:19:51 +0900

    옵션 선택 부분은 class-main.php 부분으로 던져냄.

--- includes/class-wskl-pay-gates.php
+++ includes/class-wskl-pay-gates.php
@@ -7,37 +7,34 @@ class WSKL_Pay_Gates {
 
 	public static function init() {
 
-		if ( wskl_is_option_enabled( 'enable_sym_pg' ) ) {
+		// NOTE: 옵션 켜져 있는 것과 pay_gate_agency 찾을 수 있는 것은 별개의 사항으므로 에러 체크가 필요함.
+		static::$pay_gate = get_option( wskl_get_option_name( 'pg_agency' ) );
 
-			// NOTE: 옵션 켜져 있는 것과 pay_gate_agency 찾을 수 있는 것은 별개의 사항으므로 에러 체크가 필요함.
-			static::$pay_gate = get_option( wskl_get_option_name( 'pg_agency' ) );
+		// NOTE: 이전 버전 (<=3.2.0) 기능 호환을 위해 사용. 앞으로는 글로벌 변수는 사용을 자제할 것.
+		static::export_globals();
 
-			// NOTE: 이전 버전 (<=3.2.0) 기능 호환을 위해 사용. 앞으로는 글로벌 변수는 사용을 자제할 것.
-			static::export_globals();
+		if ( static::$pay_gate && ! empty( static::$pay_gate ) ) {
 
-			if ( static::$pay_gate && ! empty( static::$pay_gate ) ) {
+			$pg_main_path   = WSKL_PATH . '/includes/lib/class-pg-' . static::$pay_gate . '-main.php';
+			$pg_common_path = WSKL_PATH . '/includes/lib/class-pg-' . static::$pay_gate . '-common.php';
 
-				$pg_main_path   = WSKL_PATH . '/includes/lib/class-pg-' . static::$pay_gate . '-main.php';
-				$pg_common_path = WSKL_PATH . '/includes/lib/class-pg-' . static::$pay_gate . '-common.php';
+			if ( file_exists( $pg_main_path ) && file_exists( $pg_common_path ) ) {
 
-				if ( file_exists( $pg_main_path ) && file_exists( $pg_common_path ) ) {
+				/** @noinspection PhpIncludeInspection */
+				require_once( $pg_main_path );
 
-					/** @noinspection PhpIncludeInspection */
-					require_once( $pg_main_path );
+				/** @noinspection PhpIncludeInspection */
+				require_once( $pg_common_path );
 
-					/** @noinspection PhpIncludeInspection */
-					require_once( $pg_common_path );
+				/**
+				 * Woocommerce REST API V3 action.
+				 *
+				 * @see \WC_API::handle_api_requests()
+				 */
+				add_action( 'woocommerce_api_request', array( __CLASS__, 'add_api_request' ) );
 
-					/**
-					 * Woocommerce REST API V3 action.
-					 *
-					 * @see \WC_API::handle_api_requests()
-					 */
-					add_action( 'woocommerce_api_request', array( __CLASS__, 'add_api_request' ) );
-
-				} else {
-					add_action( 'admin_notices', array( __CLASS__, 'output_pay_gate_error' ) );
-				}
+			} else {
+				add_action( 'admin_notices', array( __CLASS__, 'output_pay_gate_error' ) );
 			}
 		}
 	}
@@ -74,11 +71,7 @@ class WSKL_Pay_Gates {
 
 	public static function output_pay_gate_error() {
 
-		printf(
-			'<div class="notice error"><p class="wskl-warning">%s: %s</p></div>',
-			__( '다음 PG 모듈이 발견되지 않았습니다.', 'wskl' ),
-			static::$pay_gate
-		);
+		printf( '<div class="notice error"><p class="wskl-warning">%s: %s</p></div>', __( '다음 PG 모듈이 발견되지 않았습니다.', 'wskl' ), static::$pay_gate );
 	}
 }
 
