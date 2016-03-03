7f56569f6734f52b6740240320f1d62c63ad618e
changwoo <ep6tri@hotmail.com>
2016-03-03 13:25:12 +0900

    code re-format

--- includes/lib/sym-mvc/wskl-sym-mvc-framework.php
+++ includes/lib/sym-mvc/wskl-sym-mvc-framework.php
@@ -18,18 +18,18 @@ if ( ! function_exists( 'wskl_sym_custom_data_activate' ) ) :
 		global $wpdb;
 
 		// create custom order data table
-		$wpdb->query( '
-      CREATE TABLE IF NOT EXISTS sym_custom_data (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        order_id INT NOT NULL,
-        custom_key VARCHAR(32) NOT NULL,
-        custom_value TEXT
-      ) ENGINE = INNODB;
-    ' );
+		$wpdb->query(
+			'CREATE TABLE IF NOT EXISTS sym_custom_data (
+        		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        		order_id INT NOT NULL,
+        		custom_key VARCHAR(32) NOT NULL,
+        		custom_value TEXT
+      		) ENGINE = INNODB;'
+		);
 	}
 
 	// register activation hook
-	register_activation_hook( __FILE__, 'wskl_sym_custom_data_activate' );
+	register_activation_hook( WSKL_MAIN_FILE, 'wskl_sym_custom_data_activate' );
 
 endif;
 
