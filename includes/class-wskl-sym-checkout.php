dd30d6ae3e4819aa33717ab6e3c2e672428b7352
changwoo <ep6tri@hotmail.com>
2016-04-01 16:33:12 +0900

    코드 리포맷, 다보리 멤버스에서 다음 주소 모듈을 같이 쓰게 되었으므로, 이 쪽 코드 리펙터링.

--- includes/class-wskl-sym-checkout.php
+++ includes/class-wskl-sym-checkout.php
@@ -15,12 +15,16 @@ class WSKL_Sym_Checkout {
 	public static function init() {
 
 		/** 초기화 액션 */
-		add_action( 'woocommerce_init',
-		            array( __CLASS__, 'customize_checkout_address_fields' ) );
+		add_action(
+			'woocommerce_init',
+			array( __CLASS__, 'customize_checkout_address_fields' )
+		);
 
 		/** JS 삽입. */
-		add_action( 'wp_enqueue_scripts',
-		            array( __CLASS__, 'enqueue_scripts' ) );
+		add_action(
+			'wp_enqueue_scripts',
+			array( __CLASS__, 'enqueue_scripts' )
+		);
 	}
 
 	/**
@@ -36,35 +40,43 @@ class WSKL_Sym_Checkout {
 		 * @see    woocommerce/includes/class-wc-countries.php
 		 * @see    WC_Countries::get_address_fields()
 		 */
-		add_filter( 'woocommerce_billing_fields',
-		            array( __CLASS__, 'billing_address' ),
-		            10,
-		            2 );
+		add_filter(
+			'woocommerce_billing_fields',
+			array( __CLASS__, 'billing_address' ),
+			10,
+			2
+		);
 
 		/**
 		 * @filter woocommerce_{$type}fields
 		 * @see    woocommerce/includes/class-wc-countries.php
 		 * @see    WC_Countries::get_address_fields()
 		 */
-		add_filter( 'woocommerce_shipping_fields',
-		            array( __CLASS__, 'shipping_address' ),
-		            10,
-		            2 );
+		add_filter(
+			'woocommerce_shipping_fields',
+			array( __CLASS__, 'shipping_address' ),
+			10,
+			2
+		);
 
 		/**
 		 * @filter woocommerce_form_field_{$type}
-		 * @see woocommerce/includes/wc-template-functions.php
-		 * @see woocommerce_form_field()
+		 * @see    woocommerce/includes/wc-template-functions.php
+		 * @see    woocommerce_form_field()
 		 */
-		add_filter( 'woocommerce_form_field_hidden',
-		            array( __CLASS__, 'output_hidden_form_fields' ),
-		            10,
-		            4 );
+		add_filter(
+			'woocommerce_form_field_hidden',
+			array( __CLASS__, 'output_hidden_form_fields' ),
+			10,
+			4
+		);
 
-		add_filter( 'woocommerce_form_field_button',
-		            array( __CLASS__, 'output_button_form_field' ),
-		            10,
-		            4 );
+		add_filter(
+			'woocommerce_form_field_button',
+			array( __CLASS__, 'output_button_form_field' ),
+			10,
+			4
+		);
 	}
 
 	/**
@@ -82,8 +94,10 @@ class WSKL_Sym_Checkout {
 
 		switch ( $country ) {
 			case 'KR':
-				return static::address_common_korean( 'billing_',
-				                                      $address_fields );
+				return static::address_common_korean(
+					'billing_',
+					$address_fields
+				);
 		}
 
 		return $address_fields;
@@ -163,9 +177,11 @@ class WSKL_Sym_Checkout {
 			// billing_city 부분을 무시하고 address_1 을 사용.
 			'address_1'      => array(
 				'label'             => __( '주소', 'wskl' ),
-				'placeholder'       => _x( '주소 - 시/도(번지 이전까지)',
-				                           'placeholder',
-				                           'wskl' ),
+				'placeholder'       => _x(
+					'주소 - 시/도(번지 이전까지)',
+					'placeholder',
+					'wskl'
+				),
 				'required'          => TRUE,
 				'class'             => array(
 					'form-row-wide',
@@ -177,9 +193,11 @@ class WSKL_Sym_Checkout {
 			),
 
 			'address_2' => array(
-				'placeholder'       => _x( '주소 - 번지 이후',
-				                           'placeholder',
-				                           'wskl' ),
+				'placeholder'       => _x(
+					'주소 - 번지 이후',
+					'placeholder',
+					'wskl'
+				),
 				'class'             => array(
 					'form-row-wide',
 					'address-field',
@@ -264,8 +282,10 @@ class WSKL_Sym_Checkout {
 
 		switch ( $country ) {
 			case 'KR':
-				return static::address_common_korean( 'shipping_',
-				                                      $address_fields );
+				return static::address_common_korean(
+					'shipping_',
+					$address_fields
+				);
 		}
 
 		return $address_fields;
@@ -280,10 +300,12 @@ class WSKL_Sym_Checkout {
 		$value
 	) {
 
-		return sprintf( '<input type="hidden" name="%s" id="%s" value="%s" />',
-		                esc_attr( $key ),
-		                esc_attr( $key ),
-		                esc_attr( $value ) );
+		return sprintf(
+			'<input type="hidden" name="%s" id="%s" value="%s" />',
+			esc_attr( $key ),
+			esc_attr( $key ),
+			esc_attr( $value )
+		);
 	}
 
 	/**
@@ -311,9 +333,13 @@ class WSKL_Sym_Checkout {
 
 			if ( isset( $args['required'] ) && $args['required'] ) {
 				$args['class'][] = 'validate-required';
-				$required        = sprintf( '<abbr class="required" title="%s">*</abbr>',
-				                            esc_attr__( 'required',
-				                                        'woocommerce' ) );
+				$required        = sprintf(
+					'<abbr class="required" title="%s">*</abbr>',
+					esc_attr__(
+						'required',
+						'woocommerce'
+					)
+				);
 			} else {
 				$required = '';
 			}
@@ -327,18 +353,24 @@ class WSKL_Sym_Checkout {
 			$escaped_key   = esc_attr( $key );
 			$escaped_label = esc_attr( $args['label'] );
 
-			$field = sprintf( '<p class="form-row %s">',
-			                  esc_attr( implode( ' ', $args['class'] ) ) );
-			$field .= sprintf( '<label for="%s" class="%s">%s%s</label>',
-			                   $escaped_key,
-			                   esc_attr( implode( ' ', $args['label_class'] ) ),
-			                   '&nbsp;', // $escaped_label,
-			                   $required );
-			$field .= sprintf( '<input type="button" class="button" name="%s" id="%s" value="%s" /></p>%s',
-			                   $escaped_key,
-			                   $escaped_key,
-			                   $escaped_label,
-			                   $div_clear );
+			$field = sprintf(
+				'<p class="form-row %s">',
+				esc_attr( implode( ' ', $args['class'] ) )
+			);
+			$field .= sprintf(
+				'<label for="%s" class="%s">%s%s</label>',
+				$escaped_key,
+				esc_attr( implode( ' ', $args['label_class'] ) ),
+				'&nbsp;', // $escaped_label,
+				$required
+			);
+			$field .= sprintf(
+				'<input type="button" class="button" name="%s" id="%s" value="%s" /></p>%s',
+				$escaped_key,
+				$escaped_key,
+				$escaped_label,
+				$div_clear
+			);
 		}
 
 		return $field;
@@ -350,24 +382,15 @@ class WSKL_Sym_Checkout {
 	 */
 	public static function enqueue_scripts() {
 
-		// Load frontend JS & CSS
-		if ( is_ssl() ) {
-			$daum_zip_api_url = 'https://spi.maps.daum.net/imap/map_js_init/postcode.v2.js';
-		} else {
-			$daum_zip_api_url = 'http://dmaps.daum.net/map_js_init/postcode.v2.js';
-		}
+		wskl_enqueue_daum_postcode_scripts();
 
-		wp_enqueue_script( 'daum-postcode-v2-js',
-		                   $daum_zip_api_url,
-		                   NULL,
-		                   WSKL_VERSION,
-		                   FALSE );  // in the header area
-
-		wp_enqueue_script( 'daum_zipcode-js',
-		                   plugin_dir_url( WSKL_MAIN_FILE ) . 'assets/js/daum-zipcode.js',
-		                   array( 'daum-postcode-v2-js', ),
-		                   WSKL_VERSION,
-		                   TRUE );  // in the footer area
+		wp_enqueue_script(
+			'daum_zipcode',
+			plugin_dir_url( WSKL_MAIN_FILE ) . 'assets/js/daum-zipcode.js',
+			array( 'daum-postcode-v2', ),
+			WSKL_VERSION,
+			TRUE
+		);  // in the footer area
 	}
 }
 
