e3f5e500443a7cf0354907d9eed69ade9f848c14
changwoo <ep6tri@hotmail.com>
2016-04-08 10:34:35 +0900

    다보리 '실험실'에서 '베타'로 변경.

--- includes/admin/settings/lab.php
+++ includes/admin/settings/lab.php
@@ -1,8 +1,8 @@
 <?php
 
 $lab = array(
-	'title'       => __( '다보리 실험실', 'wskl' ),
-	'description' => __( '특정 기능을 위한 실험 기능', 'wskl' ),
+	'title'       => __( '다보리 베타', 'wskl' ),
+	'description' => __( '특정 기능을 위한 베타 기능', 'wskl' ),
 	'fields'      => array(
 		array(
 			'id'          => 'enable_combined_tax_kcp',
